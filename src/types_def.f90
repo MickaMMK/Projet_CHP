0e7316f25051b9ada32b02bdd80888b3e8b6c201
MickaMMK <mrivier@enseirb.fr>
2015-11-02 16:32:32 +0100

    mise en place des types

--- src/types_def.f90
+++ src/types_def.f90
@@ -1,20 +1,76 @@
-module maillage
+program main
   
   implicit none
 
-  type noeud
+  type point
      real(8), dimension(2) :: coord
-     type(maille), dimension(:), pointer :: mailles
-  end type noeud
+     type(point), pointer :: pointNE, pointNO, pointSE, pointSO
+  end type point
 
-  type centre
+  type vecteur
      real(8), dimension(2) :: coord
-     type(maille), pointer :: maille
-  end type centre
+     type(point), pointer :: point
+  end type vecteur
 
-  type maille
-     type(noeud), dimension(:), pointer :: sommets
-     type(centre), pointer :: centre
-  end type maille
+  type scalaire
+     real(8) :: valeur
+     type(point), pointer :: point
+  end type scalaire
 
-end module maillage
+  !----------------------------
+  integer, parameter :: N=20  !
+  !----------------------------
+
+  type(point), dimension(N,N), target :: centres
+  type(point), dimension(N+1,N+1), target :: noeuds
+  integer :: i, j
+
+  do i = 1, N+1
+     do j = 1, N+1
+        noeuds(i,j)%coord = (/float(i-1)/N, float(j-1)/N/)
+     end do
+  end do
+
+  do i = 1, N
+     do j = 1, N
+        centres(i,j)%coord = (/(float(i-1)+0.5)/N, (float(j-1)+0.5)/N/)
+     end do
+  end do
+
+  do i = 1, N
+     do j = 1, N
+        centres(i,j)%pointNO => noeuds(i,j+1)
+        centres(i,j)%pointNE => noeuds(i+1,j+1)
+        centres(i,j)%pointSE => noeuds(i+1,j)
+        centres(i,j)%pointSO => noeuds(i,j)
+     end do
+  end do
+
+  do i = 2, N
+     do j = 2, N
+        noeuds(i,j)%pointNO => centres(i-1,j)
+        noeuds(i,j)%pointNE => centres(i,j)
+        noeuds(i,j)%pointSE => centres(i,j-1)
+        noeuds(i,j)%pointSO => centres(i-1,j-1)
+     end do
+  end do
+
+  open(unit=12, file="noeuds.dat", status="replace")
+  open(unit=13, file="centres.dat", status="replace")
+  
+  do i = 1, N+1
+     do j = 1, N+1
+        write(12,*) noeuds(i,j)%coord
+     end do
+  end do
+
+  do i = 1, N
+     do j = 1, N
+        write(13,*) centres(i,j)%coord
+     end do
+  end do
+
+  close(12)
+  close(13)
+
+end program main
