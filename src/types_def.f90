428c2cacab92cfc7933f3e6f30a52ba282bc0116
MickaMMK <mrivier@enseirb.fr>
2015-11-02 14:50:37 +0100

    je déplace

---
+++ src/types_def.f90
@@ -0,0 +1,20 @@
+module maillage
+  
+  implicit none
+
+  type noeud
+     real(8), dimension(2) :: coord
+     type(maille), dimension(:), pointer :: mailles
+  end type noeud
+
+  type centre
+     real(8), dimension(2) :: coord
+     type(maille), pointer :: maille
+  end type centre
+
+  type maille
+     type(noeud), dimension(:), pointer :: sommets
+     type(centre), pointer :: centre
+  end type maille
+
+end module maillage
