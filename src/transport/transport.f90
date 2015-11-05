d47d8b987a8e915e0d097d45b602fd2ba8889d04
MickaMMK <mrivier@enseirb.fr>
2015-11-05 16:33:33 +0100

    TRANSPORT OPE !

--- src/transport/transport.f90
+++ src/transport/transport.f90
@@ -1,4 +1,9 @@
 module transportmod
+
+  use eulermod
+  use locatemod
+  use interpmod
+
 !!$%%%%%%% COMMENTAIRES %%%%%%%!!
 !!$Résolution de l'équation de transport d(f)/dt + v.grad(f)=0
 !!$
@@ -12,32 +17,37 @@ contains
 
     implicit none
 
-    real(8), dimension(N+1,N+1,2), intent(in) :: noeuds, vitesses
-    real(8), dimension(N+1,N+1), intent(inout) :: level
+    real(8), dimension(:,:,:), intent(in) :: noeuds, vitesses
+    real(8), dimension(:,:), intent(inout) :: level
     real(8), intent(in) :: dt, dx
-    real(8), dimension(N+1,N+1) :: old_level
+    real(8), dimension(size(level(:,1)),size(level(1,:))) :: old_level
     real(8), dimension(2) :: coord
     real(8), dimension(4,3) :: sommets
     integer, dimension(4,2) :: indices
-    integer::i,j,k
+    integer::i,j,k,N
+    real(8) :: val
 
     old_level = level
+    N = size(level(:,1))-1
 
     !boucle sur les noeuds
     do i = 1, N+1
        do j = 1, N+1
 
           !position au temps précedent
-          coord = noeuds(i,j)
+          coord = noeuds(i,j,:)
           call euler(-1.*vitesses(i,j,:), coord, dt) !Modifie coord
-          call locate(coord, dx, indices) !Récupère les indices des sommets
-          !valeur au temps précédent
-          do k = 1, 4
-             sommets(k,1:2) = noeuds(indices(k,1), indices(k,2))  !sommets contient pour les 4 sommets
-             sommets(k,3) = old_level(indices(k,1), indices(k,2)) !les coord x, y et la valeur du level
-          end do
-          call interp(sommets, coord, val) !Interpolation de la valeur du level
-          level(i,j) = val
+          if(coord(1) < 1e-6 .OR. coord(2) < 1e-6 .OR. coord(1) > 1-1e-6 .OR. coord(2) > 1-1e-6) then
+          else
+             call locate(coord, dx, indices) !Récupère les indices des sommets
+             !valeur au temps précédent
+             do k = 1, 4
+                sommets(k,1:2) = noeuds(indices(k,1), indices(k,2),:)  !sommets contient pour les 4 sommets
+                sommets(k,3) = old_level(indices(k,1), indices(k,2)) !les coord x, y et la valeur du level
+             end do
+             call interp(sommets, coord, val) !Interpolation de la valeur du level
+             level(i,j) = val
+       end if
 
        end do
     end do
