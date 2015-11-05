f98f67f3ff54904cf7b6f6d89b012d8a4be5a223
LeCorentin <cprigent@enseirb.fr>
2015-11-05 18:52:08 +0100

    rien

--- src/navierstokes/navierstokes.f90
+++ src/navierstokes/navierstokes.f90
@@ -1,23 +1,49 @@
 module navierstokes
-     use maillage
-     use vecteur_plan
      implicit none
 
      contains
 
 !méthode de projection de Chorin
-     subroutine projection_method(u,p,u_next,p_next)
+     subroutine projection_method(u,p,u_next,p_next,rho,nu,g,dt,dx,dy)
         implicit none
 
-        type(vector2D) , dimension(:,:) , intent(in) :: u
+        ! vitesse au temps n
+        real(kind=8) , dimension(:,:,2) , intent(in) :: u
         real(kind=8) , dimension(:,:) , intent(in) :: p
-
-        type(vector2D) , dimension(:,:), intent(out) :: u_next
+        
+        !vitesse au temps n+1
+        real(kind=8) , dimension(:,:,2), intent(out) :: u_next
         real(kind=8) , dimension(:,:) , intent(out) :: p_next
 
-        type(vector2D) , dimension(:,:) :: u_star
+        !vitesse intermédiaire
+        real(kind=8) , dimension(:,:) :: u_star
 
-        u_star = u 
+        !paramètres physiques
+        real(kind=8) , intent(in) :: rho , nu , g
+
+        !paramètres numériques
+        real(kind=8) , intent(in) :: dt , dx , dy
+        integer :: Nx , Ny
+
+        real(kind=8) , dimension(:,:,2) , allocatable :: laplace_u , u_grad_u
+
+        Nx = size(u,1)-1
+        Ny = size(u,2)-1
+
+        allocate(laplace_u(Nx-1,Ny-1,2),u_grad_u(Nx-1,Ny-1,2))
+
+        laplace_u = ( u(3:Nx+1,2:Ny,:) - 2*u(2:Nx,2:Ny,:) + u(1:Nx-1,2:Ny,:) ) / (dx*dx) &
+                   & + ( u(2:Nx,3:Ny+1,:) - 2*u(2:Nx,2:Ny,:) + u(2:Nx,1:Ny-1,:) ) / (dy*dy)
+        
+        u_grad_u = u(2:Nx,2:Ny,1)*( u(3:Nx+1,2:Ny,:) - u(1:Nx-1,2:Ny,:) ) / (2*dx) &
+                  & + u(2:Nx,2:Ny,2)*( u(2:Nx,3:Ny+1,:) - u(2:Nx,1:Ny-1,:) ) / (2*dy)
+
+        u_star = 0
+
+        u_star(2:Nx,2:Ny,:) = u(2:Nx,2:Ny,:) + dt*( g + nu*laplace_u - u_grad_u )
+
+        
+        deallocate(laplace_u)
 
      end subroutine projection_method
 
