7c87ccaf3589dc23e1a74e8be7f6fa3035fa58b6
MickaMMK <mrivier@enseirb.fr>
2015-11-12 20:01:52 +0100

    quelques trucs

--- src/main.f90
+++ src/main.f90
@@ -15,7 +15,8 @@ program main
   real(8), dimension(N+1,N+1) :: level
   real(8), dimension(N,N) :: pressions
   real(8) :: dx, dt
-  integer :: i, j, Niter
+  integer :: i, j, Niter, ci, di, ui, k
+  character(len=1) :: c, d, u
   
   dx = 1./N
   do i = 1, N+1
@@ -23,9 +24,9 @@ program main
         noeuds(i,j,:) = (/(i-1)*dx, (j-1)*dx/)
         vitesses(i,j,:) = (/0.3,0.3/)
         if(sqrt((noeuds(i,j,1)-0.5)**2+(noeuds(i,j,2)-0.5)**2) < 0.2) then
-           level(i,j) = 1.
-        else
            level(i,j) = 0.
+        else
+           level(i,j) = 1.
         endif
      end do
   end do
@@ -40,51 +41,43 @@ program main
   dt = cfl*dx/maxval(vitesses)
   Niter = ceiling(tmax/dt)
 
-  open(unit=22, file="noeuds.dat", status="replace")
-  open(unit=23, file="centres.dat", status="replace")
+  open(unit=22, file="noeuds000.txt", status="replace")
+
+  write(22,*) '"x", "y", "level"'
   
   do i = 1, N+1
      do j = 1, N+1
-        write(22,*) noeuds(i,j,:), level(i,j)
+        write(22,*) noeuds(i,j,1),", ", noeuds(i,j,2),", ", level(i,j)
      end do
-     write(22,*)
-  end do
-
-  do i = 1, N
-     do j = 1, N
-        write(23,*) centres(i,j,:)
-     end do
-     write(23,*)
   end do
 
   close(22)
-  close(23)
 
-  do i = 1, Niter
+  do k = 1, Niter
+
      call transport(noeuds, vitesses, level, dt, dx)
-  end do
 
-  open(unit=12, file="noeudsf.dat", status="replace")
-  open(unit=13, file="centresf.dat", status="replace")
-  
-  do i = 1, N+1
-     do j = 1, N+1
-        write(12,*) noeuds(i,j,:), level(i,j)
+     ci = int(k/100)
+     di = modulo(int(k/10),10)
+     ui = modulo(k,10)
+
+     write(c,'(I1)') ci
+     write(d,'(I1)') di
+     write(u,'(I1)') ui
+
+     open(unit=12, file="noeuds"//c//d//u//".txt", status="replace")
+
+     write(12,*) '"x", "y", "level"'
+
+     do i = 1, N+1
+        do j = 1, N+1
+           write(12,*) noeuds(i,j,1),", ", noeuds(i,j,2),", ", level(i,j)
+        end do
      end do
-     write(12,*)
+     
+     close(12)
+
   end do
-
-  do i = 1, N
-     do j = 1, N
-        write(13,*) centres(i,j,:)
-     end do
-     write(13,*)
-  end do
-
-  close(12)
-  close(13)
-
-
 
 contains
 
