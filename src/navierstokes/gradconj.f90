9d46b569d7e59de81394d75794ae0a0eea386970
MartinMMK <marcastel@plaintcontrix.pedago.ipb.fr>
2015-11-19 11:47:28 +0100

    nouveau gradconj bitch

--- src/navierstokes/gradconj.f90
+++ src/navierstokes/gradconj.f90
@@ -8,7 +8,7 @@ contains
   subroutine grad_conj_opt(A,X,B)
     implicit none
 
-    real(wp), dimension(:,:), intent(in)                  :: A
+   !real(wp), dimension(:,:), intent(in)                  :: A
     real(wp), dimension(:), intent(in)                    :: B
     real(wp), dimension(size(A,1)), intent(out)           :: X
     real(wp), dimension(:,:), allocatable                 :: C, A_modif
@@ -20,33 +20,33 @@ contains
 
     allocate(C(n,n), w(n), r(n), Aw(n))
 
-    do i = 1, n
-       C(i,i) = 100._wp / A(i,i) ! matmul(A,mathieu) = produit_mat_vect(X)
-    enddo
-
-    A_modif = matmul(C,A)
-    B_modif = matmul(C,B)
+!!$    do i = 1, n
+!!$       C(i,i) = 100._wp / A(i,i) ! matmul(A,B) = mat_vect(B)
+!!$    enddo
+!!$
+!!$    A_modif = matmul(C,A)
+!!$    B_modif = matmul(C,B)
 
     eps = 0.0001_wp
     err = 1._wp
 
     X = 1._wp
-    r = B_modif - matmul(A_modif,X)
+    r = B - matmul(A,X)
     err = sqrt(dot_product(r,r))
     w = r
-    alpha = dot_product(w,r) / dot_product(matmul(A_modif,w),r)
+    alpha = dot_product(w,r) / dot_product(mat_vect(w),r)
     i = 0
 
     do while((eps<err).and.(i<1000))
 
        i = i + 1
        X = X + alpha*w
-       r = B_modif - matmul(A_modif,X)
-       Aw = matmul(A_modif,w)
+       r = B - mat_vect(X)
+       Aw = mat_vect(w)
        err = sqrt(dot_product(r,r))
        beta = dot_product(Aw-w,r) / dot_product(Aw,w)
        w = r - beta*w
-       alpha = dot_product(w,r) / dot_product(matmul(A_modif,w),w)
+       alpha = dot_product(w,r) / dot_product(mat_vect(w),w)
 
     enddo
 
